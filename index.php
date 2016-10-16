b130fe8822168c9bba9fa95b5c2ff26836bc767d
rolies <azheema19@hotmail.com>
2016-10-17 01:29:06 +0700

    start input tag

--- index.php
+++ index.php
@@ -290,12 +290,97 @@ session_start();
                             echo $new_sort[2];
                             if (!$is_there){
 //								if ID not there than just set ID value to empty
-                                //$this->check_labelid($input_tag, $start_tag, "");
+                                $this->check_labelid($input_tag, $start_tag, "");
                             } else {
-                                //$this->check_labelid($input_tag, $start_tag, $input_tags[$is_there+1]);
+                                $this->check_labelid($input_tag, $start_tag, $input_tags[$is_there+1]);
                             }
                         }
                     }
+
+                    public function check_labelid($input_tag, $start_tag, $id_name){
+                        $yes_label = 0;
+                        if ($start_tag == 0){
+                            $end_label = $start_tag;
+                        } else {
+                            $end_label = $start_tag-1;
+                        }
+                        // Check if end is truly label, if not then add aria-label to input
+                        $check_label = substr($this->all_array[$end_label], strlen($this->all_array[$end_label])-14, strlen($this->all_array[$end_label]));
+                        if ($check_label == htmlspecialchars('</label>')){
+                            $yes_label = 1;
+                        } else {
+                            if (empty($id_name)){
+                                $myfile = fopen("tempindex.txt", "w") or die("Unable to open file!");
+                                fwrite($myfile, $start_tag." ");
+                                foreach ($input_tag as $items){
+                                    fwrite($myfile, htmlspecialchars_decode($items)." ");
+                                }
+                                fclose($myfile);
+                            } else {
+//                                $this->correcting_arr($start_tag+1, "aria-labelledby=&quot;".$id_name."&quot;");
+                            }
+                        }
+                        // When end equal to label do following
+                        if ($yes_label == 1){
+                            $start_label = 0;
+                            $indicator = 0;
+                            $label_tag = array();
+                            // Find the first index of label
+                            for ($i=$end_label-2; $i>=0; $i--){
+                                $new_label = substr($this->all_array[$i], 0, 4);
+                                if ($new_label == htmlspecialchars('<')){
+                                    $start_label = $i;
+                                    break;
+                                }
+                            }
+                            for ($j=$start_label; $j<=$end_label; $j++){
+                                array_push($label_tag, $this->all_array[$j]);
+                            }
+                            $label_tag = implode(" ", $label_tag);
+                            // Create new array based on double quote, check it one by one if it equal to ID
+                            $label_tag = explode("&quot;", $label_tag);
+                            foreach ($label_tag as $item){
+                                if ($item == $id_name){
+                                    $indicator++;
+//                                    $this->correcting_arr($start_tag+1); //Label already equal to ID
+                                    break;
+                                }
+                            }
+                            if ($indicator == 0){
+                                $new_sort = $this->subarr($this->all_array, 0, 3);
+                                $is_there = $this->is_exist('for', $start_label, $end_label, $new_sort);
+                                // When ID don't have value do
+                                if ($id_name == " "){
+                                    echo "disinyalir tak punya id";
+//									if (!$is_there){
+//										//When label don't have for
+//									} else {
+//										//When label have for
+//
+//									}
+                                }
+                                //When ID have value do
+                                else {
+                                    echo sizeof($label_tag);
+                                    if (!$is_there){ //When for doesn't exist
+                                        $this->all_array[0] = htmlspecialchars('<label for="').$id_name.htmlspecialchars('">');
+                                    } else{ //When for do exist
+                                        if (sizeof($label_tag)<=3){ // Check whether
+                                            echo "";
+                                            $this->all_array[$is_there] = htmlspecialchars('for="').$id_name.htmlspecialchars('">');
+                                        } elseif (sizeof($label_tag)>=4){
+                                            echo "2";
+                                            $this->all_array[$is_there] = htmlspecialchars('for="').$id_name.htmlspecialchars('"');
+                                        }
+                                    }
+                                    echo "<p class='bg-primary'>We have corrected your label</p>";
+//									$this->correcting_arr($start_tag+1);
+                                }
+                            }
+                        }
+                    }
+
+
 //===================   Process final array correcting and saving   ========================
                     public function input_user_value($word){
                         $one_tag = file_get_contents('tempindex.txt');
@@ -311,13 +396,13 @@ session_start();
 					public function write_to_file($true_arr, $index){
 					    $this->array_ready[$index] = implode(" ", $true_arr);
 //                        debuging mode
-                        echo $this->array_ready[$index];
+//                        echo $this->array_ready[$index];
 //                        echo "<br>".sizeof($this->array_ready)." == ".$this->array_indicator."<br>";
                         $myfile = fopen("newfile.html", "w") or die("Unable to open file!");
                         if (sizeof($this->array_ready) == $this->array_indicator){
                             ksort($this->array_ready);
                             foreach ($this->array_ready as $index=>$items){
-                                echo $index."==>".$items."<br>";
+                                echo $index."==>".$items."<br>"; //debuging purpose
                                 fwrite($myfile, htmlspecialchars_decode($items)." ");
                             }
                         } elseif ($this->array_indicator < 1){
