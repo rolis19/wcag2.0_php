90ae8dac8810758a8ee20de9ba1cef9224d82902
rolies <azheema19@hotmail.com>
2016-10-18 15:56:40 +0700

    ajax save to db work

--- js/ajax.js
+++ js/ajax.js
@@ -2,11 +2,12 @@
 // Save img tag
 $(document).on('click','#save_img',function(e) {
     var info = $('#correct_img').val();
+    var tag = $('#value').val();
     var position = $('#position_img').val();
     $.ajax({
         method: "POST",
         url: "action.php",
-        data: {name: info, index: position},
+        data: {name_img: info, index_img: position, tag: tag},
         success: function(status) {
             $( ".img" ).remove();
         }
@@ -16,11 +17,12 @@ $(document).on('click','#save_img',function(e) {
 // Save input tag
 $(document).on('click','#save_input',function(e) {
     var info = $('#correct_input').val();
+    var tag = $('#value').val();
     var position = $('#position_input').val();
     $.ajax({
         method: "POST",
         url: "action.php",
-        data: {name: info, index: position},
+        data: {name_input: info, index_input: position, tag: tag},
         success: function(status) {
             $( ".input" ).remove();
         }
